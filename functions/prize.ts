de4d4f75941ad66fbe2798a2ac3bc09d66bb81e2
Imanol Valero <Imanol.Valero@live.com>
2022-03-29 23:09:03 +0200

    Revert "refactor: change cookie communication by headers"
    This reverts commit d387adcab7de5b37ca2a5d8d729f0e1639cac63c.

--- functions/prize.ts
+++ functions/prize.ts
@@ -1,18 +1,25 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const PRICE_EFFORT: number = 4
 
 const app: express.Application = express()
 
 app.use(cors({origin: '*'}))
+app.use(cookieParser())
 
 
 app.get('/prize', (req: Request, res: Response) => {
     const times: number = (parseInt(req.cookies['times']) || 0) + 1
-    const [tries, prize] = times >= PRICE_EFFORT ? [0,'1F451'] : [times, '1F4A9']
-    res.set('times', '' + tries)
-    res.set('Access-Control-Expose-Headers', 'times')
+    const [cookie, prize] = times >= PRICE_EFFORT ? [0,'1F451'] : [times, '1F4A9']
+    res.cookie('times', '' + cookie, {
+        maxAge: 86_400_000,
+        httpOnly: true,
+        secure: true,
+        
+    });
+    res.set('Access-Control-Allow-Credentials', 'true')
     res.status(200).send(prize)
 })
 
--- public/main.js
+++ public/main.js
@@ -2,7 +2,6 @@ const url = 'https://eoscqrfgrtownzyndhvx.nhost.run'
 const peli = new Freezeframe('.film', {trigger: false,});
 let peliRunning = false;
 let prize = 0x1F4A9;
-let tries = 0;
 
 const showPrize = () =>{
     const chest = document.querySelector(".chest")
@@ -19,10 +18,7 @@ document.querySelector(".crush").addEventListener("click", () => {
         peli.start();
         setTimeout(showPrize, 850);
         fetch(`${url}/v1/functions/prize`)
-            .then(response => {
-                tries = response.headers.get('times') || 0
-                return response.text()
-            })
+            .then(data => data.text())
             .then(text => prize = parseInt(text, 16))
             .catch(_ => prize = 0x1F4A9)
     }
