e799f51161c868fd58397ab802c57e7200f41ac0
Imanol Valero <Imanol.Valero@live.com>
2022-03-30 20:58:46 +0200

    feat: show digging progress in frontend

--- functions/prize.ts
+++ functions/prize.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-const PRICE_EFFORT: number = 4
-
 const app: express.Application = express()
 
 app.use(cors({
@@ -15,7 +13,8 @@ app.use(cookieParser())
 
 app.get('/prize', (req: Request, res: Response) => {
     const times: number = parseInt(req.cookies?.times ?? '0') + 1
-    const [cookie, prize] = times >= PRICE_EFFORT ? [0,'1F451'] : [times, '1F4A9']
+    const luckyNumber = Math.floor((Math.random() * 5) + 1)
+    const [cookie, prize] = times >= luckyNumber ? [0,'1F451'] : [times, '1F4A9']
     res.cookie('times', '' + cookie, {
         maxAge: 86_400_000,
         // httpOnly: true,
--- public/main.js
+++ public/main.js
@@ -1,26 +1,29 @@
 const url = 'https://eoscqrfgrtownzyndhvx.nhost.run' 
 const peli = new Freezeframe('.film', {trigger: false,});
 let peliRunning = false;
-let prize = 0x1F4A9;
+let prize = 0x1F451;
+let gold = 0x1F451;
 
 const showPrize = () =>{
     const chest = document.querySelector(".chest")
     peli.stop();
-    chest.innerHTML = `&#${prize};`
-    chest.classList.add('visible')
+    chest.innerHTML = `&#${prize};${chest.innerHTML}`
     peliRunning = false
 }
 
 document.querySelector(".crush").addEventListener("click", () => {
     if (!peliRunning) {
-        document.querySelector(".chest").classList.remove('visible')
+        if (prize === gold)
+            document.querySelector(".chest").innerHTML = '  '
         peliRunning = true
         peli.start();
         setTimeout(showPrize, 850);
         fetch(`${url}/v1/functions/prize`, {
             credentials: 'include',
-        }).then(data => data.text()
-        ).then(text => prize = parseInt(text, 16)
+        }).then(res => {
+            times = res.cookies
+            return data.text()
+        }).then(text => prize = parseInt(text, 16)
         ).catch(_ => prize = 0x1F4A9)
     }
 });
