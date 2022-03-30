6ecf15a4a83d15ad1691024e86dc0c1076488713
Imanol Valero <Imanol.Valero@live.com>
2022-03-30 21:18:49 +0200

    fix: final error fixes

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ let prize = 0x1F451;
 let gold = 0x1F451;
 
 const showPrize = () =>{
+    console.log(prize, gold, prize == gold)
     const chest = document.querySelector(".chest")
     peli.stop();
     chest.innerHTML = `&#${prize};${chest.innerHTML}`
@@ -20,13 +21,8 @@ document.querySelector(".crush").addEventListener("click", () => {
         setTimeout(showPrize, 850);
         fetch(`${url}/v1/functions/prize`, {
             credentials: 'include',
-        }).then(res => {
-            times = res.cookies
-            return data.text()
-        }).then(text => prize = parseInt(text, 16)
-        ).catch(_ => {
-            prize = 0x1F4A9
-            console.log(_)
-        })
+        }).then(res => res.text()
+        ).then(text => prize = parseInt(text, 16)
+        ).catch(_ => prize = 0x1F4A9)
     }
 });
